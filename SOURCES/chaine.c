20c6d1a3386e27715f4d7958b287d7476d3b50d1
Stimfarmer <etienne.sapin@gmail.com>
2017-02-12 12:26:59 +0100

    Ajout .h et .c traitement des chaines

---
+++ SOURCES/chaine.c
@@ -0,0 +1,34 @@
+#include "chaine.h"
+#include <stdio.h>
+
+int is_alpha( char* chaine)
+{
+   return 0;
+}
+
+int is_alnum( char* chaine)
+{
+   return 0;
+}
+
+int is_num( char* chaine)
+{
+   return 0;
+}
+
+char* delete_end_char( char* chaine)
+{
+   char *tmp;
+   return tmp;
+}
+
+int main( int argc, char *argv)
+{
+   char *chaine_test_fonction1 = "alpha";
+   char *chaine_test_fonction2 = "alpha123";
+   char *chaine_test_fonction3 = "1234";
+   
+   printf("Test main chaine.c");
+
+   return 0;
+}
