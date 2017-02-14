d4627fe061220fef867840942487df49147cc800
Stimfarmer <etienne.sapin@gmail.com>
2017-02-14 14:00:50 +0100

    Ajout du header et du .c de cryptage/décryptage

---
+++ SOURCES/cryptage.c
@@ -0,0 +1,8 @@
+#include "cryptage.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+   printf("Test main de cryptage\n");
+   return 0;
+}
---
+++ SOURCES/cryptage.h
@@ -0,0 +1,7 @@
+#ifndef _CRYPTAGE_H_
+#define _CRYPTAGE_H_
+
+/* Ce header sert à définir les fonctions de cryptage et décryptage des fichiers présents localement sur la machine d'un utilisateur */
+
+
+#endif
