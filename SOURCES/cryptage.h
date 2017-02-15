33a3a4d254849e8a9b7bc4ee0361220f4fc34eac
Stimfarmer <etienne.sapin@gmail.com>
2017-02-15 17:16:13 +0100

    Cryptage terminé

--- SOURCES/cryptage.h
+++ SOURCES/cryptage.h
@@ -3,5 +3,8 @@
 
 /* Ce header sert à définir les fonctions de cryptage et décryptage des fichiers présents localement sur la machine d'un utilisateur */
 
+int crypt_simple(char *file_name); // fonction de cryptage d'un fichier, renvoie -1 si cryptage impossible
+
+int uncrypt_simple(char *file_name); // fonction de décryptage d'un fichier, renvoie -1 si décryptage impossible
 
 #endif
